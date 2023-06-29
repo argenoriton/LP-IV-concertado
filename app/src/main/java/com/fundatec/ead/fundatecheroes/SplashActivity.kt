8eb30148d83d962fe88d456f685840c1846fbed5
João Gabriel C <gabriel_cassuriaga@hotmail.com>
2023-06-28 22:27:15 -0300

    coisei

--- app/src/main/java/com/fundatec/ead/fundatecheroes/SplashActivity.kt
+++ app/src/main/java/com/fundatec/ead/fundatecheroes/SplashActivity.kt
@@ -1,16 +1,24 @@
 package com.fundatec.ead.fundatecheroes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.fundatec.ead.fundatecheroes.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
-   private lateinit var binding: ActivitySplashBinding
-
+    private lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
 
     }
 }
